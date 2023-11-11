2a7576d3e30d4b4e69247f7529bd6017146f11a6
clapkong <clapkong@gmail.com>
2023-11-11 15:42:02 +0900

    comment added

--- test2.py
+++ test2.py
@@ -2,13 +2,14 @@ import csv
 import numpy as np
 
 
-def get_report(report_file):
+def get_report(report_file):  # 성적표(output.csv) list로 가져오기
     with open(report_file, "r", encoding="utf-8-sig") as f:
         reader = csv.reader(f)
         data = [[c.replace('\ufeff', '') for c in row] for row in reader]
     return data
 
-def course_csv_to_np(file_name):
+
+def course_csv_to_np(file_name):  # 강의 목록을 ndarray로 가져오기
     with open(file_name, "r", encoding="utf-8-sig") as f:
         reader = csv.reader(f)
         data = [[c.replace('\ufeff', '') for c in row] for row in reader]
@@ -28,16 +29,15 @@ def create_list_of_courses(course_np, report):
     return list_of_courses
 
 
-def calc_other_requirements():
-
-    return
-
 test_data = get_report('output.csv')
 
-#Fail 성적 모두 제외
+# Fail 성적 모두 제외
 for row in test_data:
     if row[4] in ['U', 'W', 'WE', 'F', 'NP']:
         test_data.remove(row)
 
+# 응용정보공학 강의 목록을 ndarray로 가져오기
 aie_course_np = course_csv_to_np('aie_course.csv')
+
+# 성적표 주인이 들은 Fail하지 않은 강의 중 응용정보공학전공 강의 추출하기
 print(create_list_of_courses(aie_course_np, test_data))
